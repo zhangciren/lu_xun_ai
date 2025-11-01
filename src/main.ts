abb40f0bda3c8ada18985ff7479949e6dd8ca774
zhangciren <1769203735@qq.com>
2025-11-01 16:31:43 +0800

    feat: 删除初始化的无用代码

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+const app = createApp(App);
+app.mount('#app')
