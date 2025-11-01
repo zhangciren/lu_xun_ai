256f7f82e7514d6619a807109901a1ccdc7e912e
zhangciren <1769203735@qq.com>
2025-11-01 17:28:33 +0800

    feat: 引入全局样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // 引入路由
 import router from './router'
+// 引入全局样式表
+import './assets/css/normalize.css'
 
 const app = createApp(App);
 app.use(router)
