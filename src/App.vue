00c79852a4546bf4ed70bdaee9871d48f5bfd834
zhangciren <1769203735@qq.com>
2025-11-01 16:47:33 +0800

    feat: 移除无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    8778
-  </div>
-  <div class="mb-4">
     <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
   </div>
 </template>
+
+<<script setup lang="ts">
+
+</script>
+
+<<style lang="" scoped>
+
+</style>
